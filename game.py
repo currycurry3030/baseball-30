77a8b1cf78237c149b83ad2d344ac83354a583d9
bj93.park <qazws9346@gmail.com>
2024-07-10 14:57:15 +0900

    [feature] 47

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
+from game_result import GameResult
+
+
 class Game:
+    def __init__(self) -> None:
+        self.question = ""
     def guess(self, guessNumber):
         self.assert_illegal_value(guessNumber)
+        return GameResult(True, 3, 0)
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
