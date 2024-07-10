bebcf6b55cfe2c3eff584dd74e2581c6b2bd9ff9
bj93.park <qazws9346@gmail.com>
2024-07-10 15:00:10 +0900

    [refactoring] 50

--- game.py
+++ game.py
@@ -4,9 +4,11 @@ from game_result import GameResult
 class Game:
     def __init__(self) -> None:
         self.question = ""
+
     def guess(self, guessNumber):
         self.assert_illegal_value(guessNumber)
-        return GameResult(True, 3, 0)
+        if guessNumber == self.question:
+            return GameResult(True, 3, 0)
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
